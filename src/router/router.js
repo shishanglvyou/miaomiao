eb54a5cb64c0d8255fee9485d52aec00451dce3c
shishanglvyou <693365957@qq.com>
2019-06-26 22:46:34 +0800

    .

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,33 @@
 export default [
   {
+    path:'/',
+    redirect:'/movie'
+  },
+  {
     path:'/movie',
-    component:()=>import('@/views/Movie')
+    component:()=>import('@/views/Movie'),
+    children:[
+      {
+        path:'city',
+        component:()=>import('@/components/City')
+      },
+      {
+        path:'nowPlaying',
+        component:()=>import('@/components/NowPlaying')
+      },
+      {
+        path:'comingSoon',
+        component:()=>import('@/components/ComingSoon')
+      },
+      {
+        path:'search',
+        component:()=>import('@/components/Search')
+      },
+      {
+        path:'/movie',
+        redirect: '/movie/nowPlaying'
+      }
+    ]
   },
   {
     path:'/mine',
